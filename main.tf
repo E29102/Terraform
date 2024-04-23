948961f0417a55199b97c15685f6b5461ec1e2a2
E29102 <156616302+E29102@users.noreply.github.com>
2024-04-23 16:00:08 +0100

    Add files via upload

--- main.tf
+++ main.tf
@@ -1,12 +1,17 @@
 terraform {
   required_providers {
-    aws = "hashicorp/aws"
-    version = "4.53.0"
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
   }
 }
 
 provider "aws" {
   region = "us-west-2"
+  access_key = "AKIAX7RBIEQVKMQCXWOT"
+  secret_key = "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"
+
 }
 
 #Initializes our VPC (Virtual Private Cloud)
@@ -67,12 +72,13 @@ resource "aws_nat_gateway" "projectnatgw" {
 }
 #Initializes the public route table
 resource "aws_route_table" "public_route_table" {
-  vpc_id = aws_vpc.testvpc.id
+  vpc_id = aws_vpc.project_vpc.id
 
   route {
-    cidr_block = "10.1.2.0/24" #Desination IP to private Subnet
+    cidr_block = "0.0.0.0/0" #Desination IP to private Subnet
     gateway_id = aws_internet_gateway.projectigw.id
   }
+
   tags = {
     Name = "Public route table"
   }
@@ -81,16 +87,17 @@ resource "aws_route_table" "public_route_table" {
 
 #Initializes the private route table
 resource "aws_route_table" "private_route_table" {
-  vpc_id = aws_vpc.testvpc.id
+  vpc_id = aws_vpc.project_vpc.id
 
   route {
-    cidr_block = aws_eip.static_ip #Desination IP to NAT gateway
-    gateway_id = aws_nat_gateway.projectigw.id
+    cidr_block = "0.0.0.0/0" #Range for Desination IP to NAT gateway
+    gateway_id = aws_nat_gateway.projectnatgw.id #Desination itself
   }
+
   tags = {
     Name = "Private route table"
   }
-  depends_on = [aws_nat_gateway.projectigw]
+  depends_on = [aws_nat_gateway.projectnatgw]
 }
 
 #Bridging the gap between the public subnet and its routing table
@@ -105,6 +112,141 @@ resource "aws_route_table_association" "priv_to_nat" {
   subnet_id      = aws_subnet.private_subnet.id
   route_table_id = aws_route_table.private_route_table.id
   depends_on     = [aws_nat_gateway.projectnatgw]
+}
+
+
+resource "aws_security_group" "internal" {
+  name        = "Internal SG"
+  description = "SG for internal instances"
+  vpc_id      = aws_vpc.project_vpc.id
+
+  ingress {
+    description = "ALL"
+    from_port = 0
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["10.1.1.0/24"]
+  }
+  # ingress {
+  #   description = "SSH"
+  #   from_port   = 22
+  #   to_port     = 22
+  #   protocol    = "tcp"
+  #   cidr_blocks = ["10.1.1.0/24"]
+  # }
+
+
+  egress {
+    from_port   = 0
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["10.1.1.0/24"]
+  }
+  tags = {
+    Name = "Internal SG"
+  }
+}
+
+resource "aws_security_group" "external" {
+  name        = "External SG"
+  description = "SG for front facing instances"
+  vpc_id      = aws_vpc.project_vpc.id
+
+  # ingress {
+  #   description = "ALL"
+  #   from_port   = 0
+  #   to_port     = 0
+  #   protocol    = "-1"
+  #   cidr_blocks = ["0.0.0.0/0"]
+  # }
+
+  ingress {
+    description = "SSH"
+    from_port   = 0
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["10.1.2.0/24"]
+  }
+
+  egress {
+    from_port   = 22
+    to_port     = 0
+    protocol    = "tcp"
+    cidr_blocks = ["10.1.2.0/24"]
+  }
+
+  ingress {
+    description = "ALL"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+
+
+  tags = {
+    Name = "External SG"
+  }
+}
+
+resource "aws_instance" "PrivVM-FE" {
+  ami           = "ami-06e85d4c3149db26a"
+  instance_type = "t3.micro"
+  availability_zone = "us-west-2a"
+  key_name = aws_key_pair.key.key_name
+  subnet_id                   = aws_subnet.private_subnet.id
+  associate_public_ip_address = false
+  vpc_security_group_ids      = ["${aws_security_group.internal.id}"]
+
+  tags = {
+    Name = "PrivVM-FE"
+  }
+  depends_on = [aws_subnet.private_subnet, aws_security_group.internal]
+}
+
+resource "aws_instance" "PrivVM-BE" {
+  ami           = "ami-06e85d4c3149db26a"
+  instance_type = "t3.micro"
+  availability_zone = "us-west-2a"
+  key_name = aws_key_pair.key.key_name
+  subnet_id                   = aws_subnet.private_subnet.id
+  associate_public_ip_address = false
+  vpc_security_group_ids      = ["${aws_security_group.internal.id}"]
+
+  tags = {
+    Name = "PrivVM-BE"
+  }
+  depends_on = [aws_subnet.private_subnet, aws_security_group.internal]
+}
+
+
+resource "aws_instance" "PubVM" {
+  ami           = "ami-06e85d4c3149db26a"
+  instance_type = "t3.micro"
+  availability_zone = "us-west-2a"
+  key_name = aws_key_pair.key.key_name
+  subnet_id                   = aws_subnet.public_subnet.id
+  associate_public_ip_address = true
+  vpc_security_group_ids      = ["${aws_security_group.external.id}"]
+
+  tags = {
+    Name = "PubVM"
+ }
+  depends_on = [aws_subnet.public_subnet, aws_security_group.external]
+}
+
+resource "aws_key_pair" "key"{
+  public_key = file("./project_key.pub")
+  key_name = "myKey"
 }
 
 
